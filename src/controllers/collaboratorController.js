b1a10c4f1e54c3176fd0dbbd558aade99844a36f
Vitor Falcao <vitorfhcosta@gmail.com>
2019-11-11 10:41:11 -0300

    Improves collaboratos listing code

--- src/controllers/collaboratorController.js
+++ src/controllers/collaboratorController.js
@@ -4,10 +4,17 @@ const Collaborator = require('../models/Collaborator')
 
 const router = express.Router();
 
+// List all collaborators
+// TODO: Pagination
 router.get('/collaborators', async (req, res) => {
-  let collaborators = await Collaborator.find();
-  console.log(collaborators)
-  return res.send(collaborators);
+  try {
+    let collaborators = await Collaborator.find();
+    return res.send(collaborators);
+  } catch (err) {
+    return res.status(400).send({
+      error: 'Something went wrong'
+    });
+  }
 });
 
 module.exports = app => app.use('/api', router);
