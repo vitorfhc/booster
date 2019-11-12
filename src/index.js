bb462e1e861ab9ca7e7ddf5e79b4fe6aee28b7b0
Vitor Falcao <vitorfhcosta@gmail.com>
2019-11-12 11:21:39 -0300

    Add 404 response message to unknown pages

--- src/index.js
+++ src/index.js
@@ -10,5 +10,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 require('./controllers/collaboratorController')(app);
 
+app.get('*', (req, res) => {
+  res.status(404).send('Not found');
+});
+
 let port = 3000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
